7d948367cc14b7b630a0581a70fecacc1235c121
Callum C <cpcooper34@hotmail.com>
2021-03-13 21:46:26 +1300

    Inital commit: Base plugin set up

---
+++ DiscordHelper.cs
@@ -0,0 +1,25 @@
+﻿using HunterPie.Core;
+
+namespace HunterPie.Plugins
+{
+    public class DiscordHelper : IPlugin
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Game Context { get; set; }
+
+        public void Initialize(Game context)
+        {
+            Name = "DiscordHelper";
+            Description = "Performs useful interactions with a discord bot.";
+
+            Context = context;
+        }
+
+        public void Unload()
+        {
+
+        }
+
+    }
+}
